b88614926b265565b7758a26ecd625a912e470f2
Duhfenix <duhfenix17@gmail.com>
2026-08-17 10:31:37 -0300

    classe lista

---
+++ src/utils/Lista.tsx
@@ -0,0 +1,31 @@
+export const Lista = [{
+    name: 'Alien 3',
+    description: 'Em Alien 3 (1992), a tenente Ellen Ripley é a única sobrevivente após sua nave cair em um planeta-prisão desolado. Ela descobre que um Xenomorfo veio a bordo e começa a caçar os detentos locais. Sem armas de fogo, Ripley precisa unir forças com os prisioneiros para destruir a criatura mortal.',
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/KUTaNMJJBa8?si=DJhm-3H8ingWb9Uz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    imagem: 'alien-3.jpg',
+},
+{
+    name: 'Chuky 2',
+    description: 'Em Brinquedo Assassino 2 (1990), o jovem Andy vai para uma família adotiva após sua mãe ser internada. Os fabricantes do boneco o reconstroem para salvar a reputação da empresa, o que traz a alma de Chucky de volta para continuar caçando Andy.',
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/QQ578YDz-sQ?si=DnuIfclpZlQIu9Bd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    imagem: 'chuky-2.jpg',
+},
+{
+    name: 'A hora do pesadelo 2010',
+    description: 'Em A Hora do Pesadelo (2010), um grupo de adolescentes de Springwood é atormentado em seus sonhos por Freddy Krueger, um assassino desfigurado com garras de aço. Conforme os jovens morrem misteriosamente enquanto dormem, eles descobrem que compartilham uma ligação secreta do passado envolvendo o vilão.',
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/8HbF1OhDBTo?si=JoH5xEboGGA4oec-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    imagem: 'fred-2010.jpg',
+},
+{
+    name: 'Jason parte 4',
+    description: 'Em Sexta-Feira 13 - Parte 4: O Capítulo Final (1984), Jason Voorhees, o assassino mascarado, continua sua matança em Crystal Lake. Após escapar da morte, ele retorna para aterrorizar um grupo de jovens que estão acampando na área. A tensão aumenta à medida que os sobreviventes tentam escapar de seu destino mortal.',
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/IvAafmoZsNI?si=dXxu1k8va0Ew6FQa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    imagem: 'jason-4.jpg',
+},
+{
+    name:'Terrifier',
+    description: 'Em Terrifier (2016), um palhaço psicopata chamado Art aterroriza uma cidade durante a noite de Halloween. Ele persegue e mata brutalmente suas vítimas, incluindo duas mulheres que tentam sobreviver à sua fúria. O filme é conhecido por suas cenas de violência gráfica e pelo desempenho aterrorizante do ator David Howard Thornton como Art.',
+    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fN5j1MtGO2Q?si=9GKePkux1b6rJ0V8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    imagem: 'terrifier.jpg',
+}
+]
